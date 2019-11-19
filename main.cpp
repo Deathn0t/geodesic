9c7a913acd63f3c24bfd84d117590c8ef3c4c994
jeremy <jeremy.goz17@gmail.com>
2019-11-19 23:14:33 +0100

    debut propagatation window

--- main.cpp
+++ main.cpp
@@ -19,14 +19,13 @@ using namespace std;
 MatrixXd V1;           // matrix storing vertex coordinates of the input curve
 MatrixXi F1;
 
-
-
 bool key_down(igl::opengl::glfw::Viewer &viewer, unsigned char key, int modifier)
 {
   std::cout << "pressed Key: " << key << " " << (unsigned int)key << std::endl;
 
   if (key == '1')
   {
+
     viewer.data(0).clear(); // Clear should be called before drawing the mesh
     viewer.data(0).set_mesh(V1, F1);
     viewer.data(0).set_colors(Eigen::RowVector3d(0.3, 0.8, 0.3)); // update the mesh (both coordinates and faces)
@@ -49,6 +48,28 @@ void set_pc(igl::opengl::glfw::Viewer &viewer)
   viewer.data().add_points(V1, Eigen::RowVector3d(0.3, 0.8, 0.3));
 }
 
+
+
+
+
+float distance(Vector3d &v1, Vector3d &v2)
+{
+    return sqrt(pow(v2(2) - v1(2), 2) +  pow(v2(1) - v1(1), 2) +  pow(v2(0) - v1(0), 2));
+}
+
+void add_window_Q(std::queue<Window> &Q, Vector3d &vs, Vector3d &v0, Vector3d &v1,int edge_id)
+{
+  double b0 = 0.;
+  double b1 = distance(v0, v1);
+
+  double d0 = distance(vs, v0);
+  double d1 = distance(vs, v1);
+
+  double sigma = 0;
+  int dir = 0;
+  Q.push(Window(b0, b1, d0, d1, sigma, dir, edge_id,v0,v1));
+}
+
 /**
  * Initialize a queue Q with a window for each edge adjacent to source vs.
  * input:
@@ -57,83 +78,81 @@ void set_pc(igl::opengl::glfw::Viewer &viewer)
  *    E: edges of the mesh.
  * output:
  *  - Q: the queue to initialize.
+ *
+ *
  */
-
-float distance(Vector3d &v1, Vector3d &v2) 
-{ 
-    return sqrt(pow(v2(2) - v1(2), 2) +  pow(v2(1) - v1(1), 2) +  pow(v2(0) - v1(0), 2)); 
-} 
-
-void add_window_Q(std::queue<Window> &Q,Vector3d &v1, Vector3d &v2,int edge_id)
-{
-  double d0 = 0.0;
-  double d1 = distance(v1,v2);
-  double sigma = 0;
-  int dir = 0;
-  Q.push(Window(v1,v2,d0,d1,sigma,dir,edge_id));
-    
-}
-void init_Q_igl(HalfedgeDS &he,int id_vs, MatrixXd &V, std::queue<Window> &Q)
+void init_Q(HalfedgeDS &he, int id_vs, MatrixXd &V, std::queue<Window> &Q)
 {
   // TODO
-  Q = std::queue<Window>(); 
-  Vector3d vs = V1.row(id_vs);
+  Q = std::queue<Window>();
 
-  int incidentEdge =  he.getEdge(id_vs);
-	int nextEdge = he.getNext(incidentEdge);
-	Vector3d firstIncidentVertex = V1.row(he.getTarget(nextEdge));
-  add_window_Q(Q,vs,firstIncidentVertex,nextEdge);
+  Vector3d vs, v_init, v_b0, v_b1;
+  int incidentEdge, nextEdge;
 
-  nextEdge = he.getNext(he.getOpposite(nextEdge));
-	Vector3d nextIncidentVertex = V1.row(he.getTarget(nextEdge));
-  add_window_Q(Q,vs,nextIncidentVertex,nextEdge);
+  vs = V.row(id_vs);
+  incidentEdge = he.getEdge(id_vs);
+	nextEdge = he.getNext(incidentEdge);
+	v_init = V.row(he.getTarget(nextEdge));
 
-	while (nextIncidentVertex!= firstIncidentVertex)
+  nextEdge = he.getNext(nextEdge);
+	v_b1 = V.row(he.getTarget(nextEdge));
+  add_window_Q(Q, vs, v_init, v_b1, nextEdge);
+
+	while (v_init != v_b1)
 	{
-        nextEdge = he.getNext(he.getOpposite(nextEdge));         
-				nextIncidentVertex = V1.row(he.getTarget(nextEdge));
-        if(nextIncidentVertex!= firstIncidentVertex)
-        {
-          add_window_Q(Q,vs,nextIncidentVertex,nextEdge);
-        }
+        v_b0 = v_b1;
+
+        nextEdge = he.getNext(he.getOpposite(he.getNext(nextEdge)));
+				v_b1 = V.row(he.getTarget(nextEdge));
+
+        add_window_Q(Q, vs, v_b0, v_b1, nextEdge);
   }
-				
-  /*
-
-  Window cur_w;
-
-  for (int e = 0; e < E.rows(); e++)
-  {
-    
-    // D(E(e, 0), 0) = D(E(e, 0), 0) + 1;
-    // D(E(e, 1), 0) = D(E(e, 1), 0) + 1;
-    // std::cout << E(e, 0) << ", " << E(e, 1) << std::endl;
-
-    if (id_vs == E(e, 0))
-    {
-       Vector3d b0 = V1.row(E(e, 0));
-       Vector3d b1 = V1.row(E(e, 1));
-       
-       double d0 = 0.0;
-       double d1 = distance(b1,b0);
-       double sigma = 0;
-       int dir = 0;
-       cur_w = Window(b0,b1,d0,d1,sigma,dir);
-       
-       Q.push(cur_w);
-
-    }
-    
-  }
-  */
-  std::cout<<Q.size();
-  
+  // std::cout<<Q.size()<<std::endl;
 }
 
-// void init_Q_He(queue<Window> &Q, Vector3d &vs)
-// {
-//   // TODO
-// }
+void propagate_window(MatrixXd &V,HalfedgeDS &he,Window &w)
+{
+
+   // On calcule nos angles de rotation entre notre ancien repere et le nouveau
+
+    int nextEdge = he.getNext(he.getOpposite(w.get_edge_id()));
+    Vector3d u = (w.get_v0() - w.get_v1()).transpose();
+    Vector3d v = w.get_v0() - V.row(he.getTarget(nextEdge)).transpose();
+    std::cout<<"u "<<u<<std::endl;
+    std::cout<<"v "<<v<<std::endl;
+    double theta = acos(u.dot(v)/(u.norm()*v.norm()));
+    std::cout<<"theta "<<theta<<std::endl;
+    
+    // Calcul des nouveaux points b0, b1 (old window) apres rotation.
+
+    MatrixXd R(2,2);
+    R(0,0) = cos(theta); R(0,1) = -sin(theta);
+    R(1,0) = sin(theta); R(1,1) = cos(theta);
+    std::cout<<"R "<<R<<std::endl;
+
+    Vector2d new_v0 = R*Vector2d(w.get_b0(),0);
+    Vector2d new_v1 = R*Vector2d(w.get_b1(),0);
+    
+    std::cout<<"new_vo "<<new_v0<<std::endl;
+    std::cout<<"new_v1 "<<new_v1<<std::endl;
+    
+    //http://math.15873.pagesperso-orange.fr/IntCercl.html
+    /* HOW TO PROPAGATE? 
+
+    1. Transpose old b0,b1 to new bo,b1 new plane (plane of edge),
+    2. Take vector of d0, d1 from old face and intersect with new edge (vector = x=infiny, y =0)
+    3. From this vectors, find intersections corresponding to new b0,b1.
+    4. From this new b0,b1 find distance d0' , d1'.
+    5. Add d0', d1' to d0  and d1.
+    6. Compute new source using new distances and new b0,b1. 
+    7. This give the new window : new b0 to b1), new distances to source d0,d1, edge_id 
+
+    */
+
+    
+    // DO PLANE TRANSLATIONr
+    
+}
 
 /**
  * Compute exact geodesics fron a source vertex to very over vertices of the mesh.
@@ -144,34 +163,31 @@ void init_Q_igl(HalfedgeDS &he,int id_vs, MatrixXd &V, std::queue<Window> &Q)
  * output:
  *    ...: something with all windows computed to apply backtracing?
  */
-void exact_geodesics_igl(HalfedgeDS &he,MatrixXd &V, MatrixXi &F, int id_vs)
+void exact_geodesics(HalfedgeDS &he,MatrixXd &V, MatrixXi &F, int id_vs)
 {
-  
+
   // initialize the queue Q with a window for each edge adjacent
   // to source: source_v_t
   std::queue<Window> Q; // Q.push(..); Q.front(); Q.pop();
-  //Window cur_w;         // current window during iterations
-  //MatrixXi E;
-  //igl::edges(F, E);
-  //std::cout<<E; //! computed once here for efficiency
+  init_Q(he,id_vs, V, Q);
 
-  init_Q_igl(he,id_vs, V, Q); // init Q with libigl DS
+  Window cur_w;         // current window during iterations
 
-   
-/*
   while (!Q.empty())
   {
     // select and remove a Window from Q
     cur_w = Q.front();
     Q.pop();
-
+    
+    propagate_window(V,he,cur_w);
+    return;
     // propagate selected window
     // TODO
 
     // update queue with new windows
     // TODO
   }
-  */
+
 }
 
 /**
@@ -191,12 +207,11 @@ void retrieve_path(Vector3d &vs, Vector3d &ve)
 void example_1()
 {
   igl::readOFF("../data/star.off", V1, F1);
-   
   int vs = 0;
   HalfedgeBuilder *builder = new HalfedgeBuilder();
-  HalfedgeDS he = (builder->createMeshWithFaces(V1.rows(), F1)); 
-   
-  exact_geodesics_igl(he,V1, F1, vs);
+  HalfedgeDS he = (builder->createMeshWithFaces(V1.rows(), F1));
+
+  exact_geodesics(he, V1, F1, vs);
 
   igl::opengl::glfw::Viewer viewer;
   set_meshes(viewer,V1,F1);
--- window.h
+++ window.h
@@ -6,23 +6,63 @@ using namespace std;
 class Window
 {
 private:
-    Vector3d b0, b1;
+    double b0, b1;
     double d0, d1;
     double sigma; // geodesic distance from s to the source vs
-    int dir; 
-    int edge_id;    // direction of s fron the directed edge e
+    int dir;
+    int edge_id;
+    Vector3d v0;
+    Vector3d v1;
+        // direction of s fron the directed edge e
 
 public:
     Window() {}
 
-    Window(Vector3d b0, Vector3d b1, double d0, double d1, double sigma, int dir, int edge_id)
+    Window(double f_b0, double f_b1, double f_d0, double f_d1, double f_sigma, int f_dir, int f_edge_id,Vector3d &f_v0,Vector3d &f_v1)
     {
-        b0 = b0;
-        b1 = b1;
-        d0 = d0;
-        d1 = d1;
-        sigma = sigma;
-        dir = dir;
-        edge_id = edge_id;
+        b0 = f_b0;
+        b1 = f_b1;
+        d0 = f_d0;
+        d1 = f_d1;
+        sigma = f_sigma;
+        dir = f_dir;
+        edge_id = f_edge_id;
+        v0 = f_v0;
+        v1 = f_v1;
+    }
+
+    int get_edge_id()
+    {
+        return edge_id;
+    }
+
+    int get_dir()
+    {
+        return dir;
+    }
+
+    double get_sigma()
+    {
+        return sigma;
+    }
+
+    double get_b0()
+    {
+        return b0;
+    }
+
+    double get_b1()
+    {
+        return b1;
+    }
+
+    Vector3d get_v0()
+    {
+        return v0;
+    }
+
+    Vector3d get_v1()
+    {
+        return v1;
     }
 };
